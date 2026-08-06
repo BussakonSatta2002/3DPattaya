58f1af5fd682c8d48436403fc8df8ac275ac2e26
Bussakon <124349461+BussakonSatta2002@users.noreply.github.com>
2026-08-06 09:20:58 +0700

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // 🚀 เพิ่มบรรทัดนี้: ต้องตรงกับชื่อ Repository บน GitLab ของคุณ (มี Slash ปิดหน้าหลัง)
-  base: '/pattaya-3d-map/', 
+  base: '/3DPattaya/', 
 
   server: {
     headers: {
